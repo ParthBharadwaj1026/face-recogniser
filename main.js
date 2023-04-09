c9d0b093d4dd69d4b7246ef809acb255e9605aeb
ParthBharadwaj1026 <86718916+ParthBharadwaj1026@users.noreply.github.com>
2023-04-09 15:52:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,30 @@ Webcam.attach('#camera');
 
 function capture_img(){
     Webcam.snap(function(data_uri){
-        document.getElementById('result').innerHTML='<img id="result_img" src="'+data_uri+'">';
+        document.getElementById('result').hidden=false;
+        document.getElementById('result').innerHTML="<img id='result_image' src='"+data_uri+"'>";
     });
+    console.log('captured');
 }
 
 console.log('ml5 version=',ml5.version);
-
-//model link to be added
 classfier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GpjatgLwS/model.json',modelloaded);
 
 function modelloaded() {
     console.log("model loaded");
+}
+
+function identfy_img(){
+    img = document.getElementById('result_image');
+    classfier.classify(img, gotresult)
+}
+
+function gotresult(error,result){
+ if (error) {
+    console.error(error);
+ } else {
+    console.log(result);
+    document.getElementById('result_person_name').innerHTML=result[0].label;
+    document.getElementById('obj_accuracy').innerHTML=result[0].confidence.toFixed(3);
+ }        
 }
